6f29f3c2264ad215164363817e2ada1b97ed9674
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-23 11:43:33 -0600

    Make boxes example look nice

--- js/boxes.js
+++ js/boxes.js
@@ -1,14 +1,30 @@
 var counter = 0;
 
-$(document).click(function(e) {
+function randX() {
+	return Math.random() * (jQuery('body').width() - 100);
+}
 
+function randY() {
+	return Math.random() * (jQuery('body').height() - 100);
+}
+
+function shake() {
+	jQuery(this).css({left: randX(), top: randY()});
+}
+
+function addBox(x, y) {
 	counter++;
+	jQuery('<div id="d' + counter + '"></div>').appendTo('.article');
+	jQuery('#d' + counter).css({left: x, top: y});
+	jQuery('#d' + counter).mouseenter(shake);
+}
 
-	var x = e.pageX;
-	var y = e.pageY;
+jQuery(document).click(function(event) {
+	addBox(event.pageX, event.pageY);
+});
 
-	$('<div id="d' + counter + '"></div>').appendTo("body");
-	$("#d" + counter).css({left: x, top: y});
-
-	$("#d" + counter).mouseenter(shake);
+jQuery('#add-100').click(function(event) {
+	for (var i = 0; i < 100; i++) {
+		addBox(randX(), randY());
+	}
 });
