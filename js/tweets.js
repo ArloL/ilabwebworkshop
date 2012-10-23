1bd7c57b487e639bf8a75ebeac81d67f9bfbf47a
Arlo O'Keeffe <mailtoarlo@gmail.com>
2012-10-23 12:03:00 -0600

    Add paragraph for tweets instead of li

--- js/tweets.js
+++ js/tweets.js
@@ -2,17 +2,20 @@ jQuery('#query').change(function(event) {
 	var query = jQuery(this).val();
 
 	var baseURL = 'http://search.twitter.com/search.json?';
-	var baseURL = baseURL + 'lang=en&';
+	baseURL += 'lang=en&';
 	jQuery.getJSON(
 		baseURL + 'q=' + query + '&callback=?',
 		function(data) {
+			jQuery('.tweet').remove();
 			var tweets = data.results;
-			var resultsList = jQuery('#results');
+			var content = jQuery('.article');
 			for (var i = tweets.length - 1; i >= 0; i--) {
 				var tweet = tweets[i];
-				jQuery(
-					'<li id="tweet' + tweet.id_str + '">' + tweet.from_user + ': ' + tweet.text + '</li>'
-				).appendTo(resultsList);
+				var tweetDiv = '<p class="tweet" id="tweet' + tweet.id_str + '">';
+				tweetDiv += '<img src="' + tweet.profile_image_url + '">'
+				tweetDiv += tweet.from_user + ': ' + tweet.text;
+				tweetDiv += '</p>';
+				jQuery(tweetDiv).appendTo(content);
 			};
 		}
 	);
